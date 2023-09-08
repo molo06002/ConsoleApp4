89c68cb064276f25253ee7a46e156c48c51c1ee7
molo06002 <molo06002@E-PW0234KQ>
2023-09-08 10:59:23 +0200

    Add project files.

---
+++ ConsoleApp4/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+namespace Variabler
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string Test1 = "Du skrev talet 5!";
+            string Text = "Skriv 5";
+            Console.WriteLine(Text);
+            string Test = Console.ReadLine();
+            int test = Convert.ToInt32(Test);  
+            if (test == 5);
+            {
+                Console.WriteLine(Test1);
+            }
+        }
+    }
+}
